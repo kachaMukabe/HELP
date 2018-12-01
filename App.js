f33b539376d7d8c903882ae14843aad3e6f96304
Kacha Mukabe <kachamukabe@gmail.com>
2018-12-01 16:39:36 +0200

    app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Overlay, ThemeProvider } from "react-native-elements";
+// import { Overlay, ThemeProvider } from "react-native-elements";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import Forum from "./components/forum";
 import Home from './components/homePage'
@@ -7,9 +7,7 @@ import Home from './components/homePage'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
         <Home />
-      </View>
     );
   }
 }
