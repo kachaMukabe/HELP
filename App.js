e99d2ebd499b557d0c295f474c04d97d6ff622de
abednegotm <abednegomwanza@gmail.com>
2018-12-01 15:56:06 +0200

    added forum title

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Overlay, ThemeProvider } from "react-native-elements";
+import { StyleSheet, Text, View } from "react-native";
+import Forum from "./components/forum";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Forum />
       </View>
     );
   }
@@ -14,8 +16,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
