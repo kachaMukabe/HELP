6ee7ed786d138f82cc8f6ac32f4d928639935d93
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-12-01 16:38:25 +0200

    Fixed errors

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Overlay, ThemeProvider } from "react-native-elements";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import Forum from "./components/forum";
 import Home from './components/homePage'
@@ -7,9 +6,7 @@ import Home from './components/homePage'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Home />
-      </View>
+      <Home />
     );
   }
 }
