2ef1126735f0a13e1fb269178db978453cc37f07
Maria Situmbeko <mariasitumbeko@gmail.com>
2018-12-01 16:01:17 +0200

    Added folder structure

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import Home from '/components/homePage'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.button}
-          // onPress={this.onPress}
-        >
-          <Text style={{color: 'white', fontSize: 24}}>PANIC!</Text>
-        </TouchableOpacity>
+        <Home />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    borderWidth:1,
-    borderColor:'rgba(0,0,0,0.2)',
-    alignItems:'center',
-    justifyContent:'center',
-    width:200,
-    height:200,
-    backgroundColor:'#d41515',
-    borderRadius:100,
-  },
-});
